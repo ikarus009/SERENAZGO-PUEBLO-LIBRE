45aba0cec1e1010f944235770c96f4b033d0bdf4
Ismael Sacha <39897987+ikarus009@users.noreply.github.com>
2024-09-09 08:21:33 -0500

    Add files via upload

---
+++ replit.nix
@@ -0,0 +1,27 @@
+{pkgs}: {
+  deps = [
+    pkgs.xsimd
+    pkgs.libxcrypt
+    pkgs.tk
+    pkgs.tcl
+    pkgs.qhull
+    pkgs.gtk3
+    pkgs.gobject-introspection
+    pkgs.ghostscript
+    pkgs.ffmpeg-full
+    pkgs.cairo
+    pkgs.glibcLocales
+    pkgs.portmidi
+    pkgs.pkg-config
+    pkgs.libpng
+    pkgs.libjpeg
+    pkgs.freetype
+    pkgs.fontconfig
+    pkgs.SDL2_ttf
+    pkgs.SDL2_mixer
+    pkgs.SDL2_image
+    pkgs.SDL2
+    pkgs.espeak-ng
+    pkgs.geos
+  ];
+}
